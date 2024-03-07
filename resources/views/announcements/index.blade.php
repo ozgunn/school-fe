7fce8c8c157f6ba8706d090d2fa79dcac1678c28
Ozgun <ozgunn@gmail.com>
2024-03-07 20:47:22 +0300

    datatable tr problem fix

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -12,7 +12,7 @@ $groups = session('groups');
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">{{ trans('Announcements') }}</h1>
-        <a href="{{ route('announcements.create') }}" class="btn btn-primary shadow-sm"><i
+        <a href="#" class="btn btn-primary shadow-sm"><i
                 class="fas fa-plus fa-fw text-white"></i> {{trans('Create')}}</a>
     </div>
 
